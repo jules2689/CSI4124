24129e00afaa5fb97d587be7a495d39e913fc899
kanyiH <khuan019@uottawa.ca>
2014-11-20 05:19:34 +0800

    Update Tracks.java

--- src/simModel/Tracks.java
+++ src/simModel/Tracks.java
@@ -7,23 +7,23 @@ public class Tracks {
 	// protected int trackSegID;
 	// protected int travelTime;
 
-	protected ArrayList<Trains> tracks = new ArrayList<Trains>();
+	protected ArrayList<Trains> trainList = new ArrayList<Trains>();
 
 	// Required methods to manipulate the queue
 	protected void spInsertQue(Trains train) {
-		tracks.add(train);
+		trainList.add(train);
 	}
 
 	protected Trains spRemoveQue() {
-		if (tracks.size() > 0) {
-			return tracks.remove(0);
+		if (trainList.size() > 0) {
+			return trainList.remove(0);
 		} else {
 			return null;
 		}
 	}
 
 	protected int getN() {
-		return tracks.size();
+		return trainList.size();
 	}
 
 }
